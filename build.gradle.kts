47211308d333f9e5162d03719c7aef2cdb725647
Evan Saulpaugh <evan.saulpaugh@gmail.com>
2022-01-03 20:00:00 -0600

    add missing parentheses

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ import java.time.format.DateTimeFormatter
 plugins {
     id("java-library")
     id("maven-publish")
-    id("me.champeau.jmh") version "0.6.6"
+    id("me.champeau.jmh").version("0.6.6")
 }
 
 group = "com.esaulpaugh"
@@ -13,50 +13,50 @@ version = "5.6.2-SNAPSHOT"
 
 project.ext.set("archivesBaseName", "headlong")
 
-tasks.withType<JavaCompile> {
+tasks.withType<JavaCompile>({
     options.compilerArgs.addAll(arrayOf("--release", "17"))
     options.encoding = "UTF-8"
-}
+})
 
-tasks.withType<Test> {
+tasks.withType<Test>({
     maxParallelForks = Runtime.getRuntime().availableProcessors()
     useJUnitPlatform()
-}
+})
 
 tasks {
-    val sourcesJar by creating(Jar::class) {
+    val sourcesJar by creating(Jar::class, {
         dependsOn(JavaPlugin.CLASSES_TASK_NAME)
         archiveClassifier.set("sources")
         from(sourceSets["main"].allSource)
-    }
+    })
 
-    val javadocJar by creating(Jar::class) {
+    val javadocJar by creating(Jar::class, {
         dependsOn(JavaPlugin.JAVADOC_TASK_NAME)
         archiveClassifier.set("javadoc")
         from(JavaPlugin.JAVADOC_TASK_NAME)
         finalizedBy("sourcesJar")
-    }
+    })
 
-    artifacts {
+    artifacts({
         add("archives", sourcesJar)
         add("archives", javadocJar)
-    }
+    })
 }
 
 val buildDate : String = ZonedDateTime.now(ZoneId.of("UTC")).format(DateTimeFormatter.ofPattern("MMMM d yyyy z"))
 
-tasks.withType<Jar> {
-    manifest {
+tasks.withType<Jar>({
+    manifest({
         attributes(
                 Pair<String, Any?>("Implementation-Title", project.name),
                 Pair<String, Any?>("Implementation-Version", project.version),
                 Pair<String, Any?>("Automatic-Module-Name", project.name),
                 Pair<String, Any?>("Build-Date", buildDate)
         )
-    }
-}
+    })
+})
 
-publishing {
+publishing({
     publications {
         create<MavenPublication>("maven") {
             groupId = "com.esaulpaugh"
@@ -67,22 +67,22 @@ publishing {
             artifact("javadocJar")
         }
     }
-}
+})
 
-repositories {
+repositories({
     mavenCentral()
-}
+})
 
 val junitVersion = "5.8.2"
 val jmhVersion = "1.33"
 val bcVersion = "1.69"
 
-dependencies {
+dependencies({
     implementation("com.google.code.gson:gson:2.8.9")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
     testImplementation("org.bouncycastle:bcprov-jdk15on:$bcVersion")
 
-    jmh("commons-codec:commons-codec:1.15")
-}
+    jmhImplementation("commons-codec:commons-codec:1.15")
+})
